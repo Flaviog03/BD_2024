82d29ed341b2483b4b5fcf7d164e915354063f43
Flaviog <grammaticof3@gmail.com>
2024-05-30 16:16:01 +0200

    Create Esercizio_3.sql

---
+++ Quaderni/Quaderno 3/Esercizio_3.sql
@@ -0,0 +1,24 @@
+/*
+CLIENTE (*CodCliente, NomeC, Cognome, Città)
+NEGOZIO (*CodNegozio, NomeN, Indirizzo, Città)
+ORDINE (*CodNegozio, *CodCliente, Data, Importo)
+
+Per ogni negozio che ha ricevuto un numero di ordini pari al massimo numero di ordini dei negozi 
+della stessa città, visualizzare il nome del negozio, la città del negozio e, 
+per ciascun ordine ricevuto, il nome e il cognome del cliente che ha effettuato l'ordine,
+la data dell'ordine e l'importo dell'ordine.
+*/
+WITH ordiniPerCittà AS (
+    SELECT Città, COUNT(O.CodNegozio, O.CodCliente) AS numeroOrdini
+    FROM ORDINE O
+    JOIN NEGOZIO N ON N.CodNegozio = O.CodNegozio
+    GROUP BY Città),
+SELECT N.NomeN, N.Città, C.NomeC, C.Cognome, O.Data, O.Importo
+FROM ORDINE O
+JOIN NEGOZIO N ON N.CodNegozio = O.CodNegozio
+JOIN CLIENTE C ON C.CodCliente = O.CodCliente
+GROUP BY N.CodNegozio, N.NomeN, N.Città, C.CodCliente, C.NomeC, C.Cognome, O.Data, O.Importo
+HAVING COUNT(O.CodNegozio, O.CodCliente) = (
+    SELECT MAX(numeroOrdini)
+    FROM ordiniPerCittà CTE1
+    WHERE CTE1.Città = N.Città);
